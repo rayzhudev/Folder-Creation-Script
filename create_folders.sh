a491f6698c8000c418e55e009a13c233d89c69af
Ray Zhu <rayzhuau@gmail.com>
2018-12-31 12:27:53 +1100

    Update script with comments for modifications
    Adds descriptive comments to indicate how to modify the script for personal use.

--- create_folders.sh
+++ create_folders.sh
@@ -1,7 +1,12 @@
 dir="$1"
-mkdir "$dir/Lecture Notes"
+#change the text in the quotes after the $/dir for a different folder name
+#e.g. mkdir "$dir/Lectures" or "$dir/Labs"
+#Note. you will need to change the text in line 10 as well if you change the folder name of the Tutorials folder
+mkdir "$dir/Lecture Notes" 
 mkdir "$dir/Tutorials"
-for i in {1..13}; do
+#edit the numbers in {1..13} to change the number of folders created
+#e.g. for i in {1..10} will create 10 folders instead of 13
+for i in {1..13}; do 
     mkdir "$dir/Tutorials/Week $i"
 done
 echo "Completed"
